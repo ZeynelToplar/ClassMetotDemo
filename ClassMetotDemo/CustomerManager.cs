15f00e036fed58b7510b5e6e242539e31ce09749
Zeynel Toplar <zt.2601@hotmail.com>
2021-05-14 01:59:44 +0300

    Demo

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekle(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Müşteri Eklendi : " + customer.Adi + " " + customer.Soyadi);
+            }
+            Console.WriteLine("-----------------");
+        }
+
+        public void Listele(Customer[] customers)
+        {
+            Console.WriteLine("\nMüşteriler Listesi :\n");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Adı : " + customer.Adi + "\nSoyadı : " + customer.Soyadi + "\nTC No : " + customer.TcNo);
+                Console.WriteLine("-----------------");
+
+            }
+
+        }
+
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi : " + customer.TcNo);
+            Console.WriteLine("-----------------");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer1 = new Customer();
+            customer1.Adi = "Mehmet Ali";
+            customer1.Soyadi = "ASİL";
+            customer1.TcNo = "1000000";
+
+            Customer customer2 = new Customer();
+            customer2.Adi = "Osman";
+            customer2.Soyadi = "ÇALIŞKAN";
+            customer2.TcNo = "1111111";
+
+            CustomerManager customer = new CustomerManager();
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            customer.Ekle(customers);
+
+            customer.Sil(customer1);
+
+            customer.Listele(customers);
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
